04c99a4727d0cdf9e905be4643e1a416582cbee0
rghosh08 <rajat.ghosh@nutanix.com>
2022-04-13 03:04:43 -0700

    add code

---
+++ rl.py
@@ -0,0 +1,26 @@
+class XPLAY_RL(object):
+    def __init__(self, metrics_api, agg_stat, action_space) -> None:
+        '''
+            metrics_api: similar to cloudwatch boto3
+            agg_stat: mean, max, and other statistical functions
+            action_space: list of all action points such as memory size
+        '''
+        self.metrics_api = metrics_api
+        self.agg_stat = agg_stat
+        self.action_space = action_space
+        
+
+    def policy(self, time_interval, threshold, current_action, action_delta):
+        '''
+            time_interval: time interval for metric compilation
+            threshold: critical value for compliance
+            current_action: current value for action point
+            action_delta: change unit for action point
+            
+        '''
+        current_metrics = self.metric_api(time_interval)
+        if self.agg_stat(current_metrics) < threshold:
+            return current_action
+        else:
+            current_action -= action_delta
+            current_metrics = self.metric_api(time_interval)
